817d200f92994a7c421bd453fd75082364f02cdf
sangavi <sangavi.vijayaraghavan@axesstechnologymail.in>
2024-05-23 11:18:15 +0530

    new

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,17 +1,18 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 import "./Header.scss";
-import { MdFoodBank} from "react-icons/md";
-import { IoMdMenu} from "react-icons/io";
+import { MdFoodBank } from "react-icons/md";
+import { IoMdMenu } from "react-icons/io";
 import { useSidebarContext } from '../../context/sidebarContext';
+import logo from '/Resturant/src/assets/images/logo.png'
 
 const Navbar = () => {
-  const {openSidebar} = useSidebarContext();
+  const { openSidebar } = useSidebarContext();
   const [scrolled, setScrolled] = useState(false);
 
   const handleScroll = () => {
     const offset = window.scrollY;
-    if(offset > 60){
+    if (offset > 60) {
       setScrolled(true);
     } else {
       setScrolled(false);
@@ -23,23 +24,22 @@ const Navbar = () => {
   })
 
   return (
-    <nav className={`navbar bg-orange flex align-center ${scrolled ? 'scrolled': ""}`}>
+    <nav className={` bg-orange flex align-center ${scrolled ? 'scrolled' : ""}`}>
       <div className='container w-100'>
         <div className='navbar-content text-white'>
           <div className='brand-and-toggler flex align-center justify-between'>
-            <Link to = "/" className='navbar-brand fw-3 fs-22 flex align-center'>
-              <MdFoodBank />
-              <span className='navbar-brand-text fw-7'>FastEat.</span>
-            </Link>
-            <div className='navbar-btns flex align-center'>
-              <button type = "button" className='navbar-show-btn text-white' onClick={() => openSidebar()}>
-                <IoMdMenu size = {27} />
-              </button>
-            </div>
+            <Link to="/" className='navbar-brand fw-3 fs-22 flex align-center'>
+              <img src={logo} alt="logo" class="logo1" />
+          </Link>
+          <div className='navbar-btns flex align-center'>
+            <button type="button" className='navbar-show-btn text-white' onClick={() => openSidebar()}>
+              <IoMdMenu size={27} />
+            </button>
           </div>
         </div>
       </div>
-    </nav>
+    </div>
+    </nav >
   )
 }
 
